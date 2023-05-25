705a78fb8202f03765d9005368b7b38cf7a13b40
SerhiiMischenko <serj.roshen@gmail.com>
2023-05-25 10:07:23 +0300

    Create Spring boot app with dependency

---
+++ src/test/java/com/app_helper/TaskHelper/TaskHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app_helper.TaskHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
